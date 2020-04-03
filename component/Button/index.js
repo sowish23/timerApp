8548582f56759e6b3c5607de4614b9ee3e6a74af
sowish23 <soso2266@naver.com>
2020-04-03 17:28:51 +0900

    button component 추가

---
+++ component/Button/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Proptypes from 'prop-types';
+import { View, TouchableOpacity, } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
+
+
+function Button({iconName, onPress}) {
+    return (
+        <TouchableOpacity onPressOut={onPress}>
+            <FontAwesome name={iconName} size={80} color="white" />
+        </TouchableOpacity>
+    )
+}
+
+Button.propTypes = {
+    iconName : Proptypes.string.isRequired,
+    onPress: Proptypes.func.isRequired,
+}
+
+export default Button;
--- component/Timer/index.js
+++ component/Timer/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import Button from '../Button';
 
 class Timer extends Component {
   render() {
@@ -9,7 +10,8 @@ class Timer extends Component {
           <Text style={styles.time}>25:00</Text>
         </View>
         <View style={styles.lower}>
-          <Text>Buttons Here</Text>
+          <Button iconName="play-circle" onPress={() => alert('itworks')} />
+          <Button iconName="stop-circle" onPress={() => alert('itworks')}  />
         </View>
       </View>
     )
